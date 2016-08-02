6639288ba9d2c204ad7086e976b06cf1ebf9c18d
Andy  Alameddine <andya@bixly.com>
2016-08-02 08:25:58 -0700

    basic spa

--- 2-react-router/src/js/pages/Layout.js
+++ 2-react-router/src/js/pages/Layout.js
@@ -1,11 +1,24 @@
-import React from "react";
+// > webpack-dev-server --content-base src
 
+import React from "react";
+import { Link } from "react-router";
 
 
 export default class Layout extends React.Component {
+  navigate() {
+    this.props.history.pushState(null, "/");
+    //replaceState does the same thing but no back button functionality
+  }
+
   render() {
     return (
-    <div>Killer News</div>
+      <div>
+        <h1>Killer News</h1>
+        {this.props.children}
+        <Link to="archives"><button class="btn btn-danger">archives</button></Link>
+        <Link to="settings"><button class="btn btn-success">settings</button></Link>
+        <button onClick={this.navigate.bind(this)} class="btn btn-primary">featured</button>
+      </div>
     );
   }
 }
