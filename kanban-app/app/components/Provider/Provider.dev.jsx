1e1d9983b0849cdb0c747c586f36c4d457c5c989
Andy  Alameddine <andyalam@hotmail.com>
2016-08-20 11:03:32 -0700

    passing in empty setup for later

--- kanban-app/app/components/Provider/Provider.dev.jsx
+++ kanban-app/app/components/Provider/Provider.dev.jsx
@@ -4,7 +4,7 @@ import chromeDebug from 'alt-utils/lib/chromeDebug';
 import alt from '../../libs/alt';
 import setup from './setup';
 
-
+setup(alt);
 chromeDebug(alt);
 
 React.Perf = require('react-addons-perf');
--- kanban-app/app/components/Provider/Provider.prod.jsx
+++ kanban-app/app/components/Provider/Provider.prod.jsx
@@ -3,7 +3,7 @@ import AltContainer from 'alt-container';
 import alt from '../../libs/alt';
 import setup from './setup';
 
-
+setup(alt);
 
 export default ({children}) => (
   <AltContainer flux={alt}>
