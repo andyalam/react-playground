c55ee000da087121bbc30764b6bdcf36aac4f222
andy <andyalam@hotmail.com>
2016-08-21 09:27:07 -0700

    porting over portfolio site to remake in react

---
+++ portfolio-remade-in-react/src/js/client.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<div><h1>Basic template working!</h1><i class="fa fa-font-awesome" aria-hidden="true"></i></div>, app);
