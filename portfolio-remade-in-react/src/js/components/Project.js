c5c0d3b19b681e9cfe723d6daeae26f0b1202fa0
andy <andyalam@hotmail.com>
2016-08-21 11:32:54 -0700

    cleaned up project component and fixed linking

--- portfolio-remade-in-react/src/js/components/Project.js
+++ portfolio-remade-in-react/src/js/components/Project.js
@@ -7,18 +7,23 @@ export default class Project extends React.Component {
     return (
       <div class="col-sm-4">
           <h3>{info.title}</h3>
-          <div class="img-box img-responsive">
+          <div class={info.title.toLowerCase().replace(/ /g,'') + " img-box img-responsive"}>
             <div class="overlay">
-              <a href="{info.linkGithub}" target="_blank">
-                <i class="fa fa-folder-open" aria-hidden="true"></i>
-              </a>
-              <a href="{info.linkDemo}" target="_blank">
-                <i class="fa fa-external-link" aria-hidden="true"></i>
-              </a>
+              { info.linkGithub &&
+                <a href={info.linkGithub} target="_blank">
+                  <i class="fa fa-folder-open" aria-hidden="true"></i>
+                </a> }
+              { info.linkDemo &&
+                <a href={info.linkDemo} target="_blank">
+                  <i class="fa fa-external-link" aria-hidden="true"></i>
+                </a> }
             </div>
           </div>
           <h4>{info.techStackUsed}</h4>
-          <a href="{info.linkGithub}" target="_blank"><h5>Github repository</h5></a>
+          { info.linkGithub &&
+            <a href={info.linkGithub} target="_blank"><h5>Github repository</h5></a> }
+          { info.linkDemo &&
+            <a href={info.linkDemo} target="_blank"><h5>Demo!</h5></a> }
           <p>{info.description}</p>
       </div>
     )
