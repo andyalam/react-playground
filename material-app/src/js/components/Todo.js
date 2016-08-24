417ae04b1d5c57349db9fac2e16b6ad91079dd30
andy <andyalam@hotmail.com>
2016-08-24 16:17:24 -0700

    "added state to todo component and enabled it switching to a text input on click"

--- material-app/src/js/components/Todo.js
+++ material-app/src/js/components/Todo.js
@@ -4,12 +4,41 @@ import { ListItem } from 'material-ui/List';
 export default class Todo extends React.Component {
   constructor(props) {
     super();
+    this.state = {
+      edit: false
+    }
+  };
+
+  enableEdit() {
+    this.setState({
+      edit: true
+    });
   }
-  
+
+  checkKeyPress(e) {
+    if (e.key === "Enter") {
+      console.log("enter");
+    }
+    console.log(e.key);
+  }
+
   render() {
     const { complete, edit, text } = this.props;
+    const icon = complete ? "\u2714" : "\u2716";
+
+    if (this.state.edit) {
+      return (
+          <ListItem
+            primaryText={<input type="text" onKeyPress={() => {this.checkKeyPress.bind(this)}}/>}
+          />
+      );
+    }
+
     return (
-      <ListItem primaryText={text}/>
+      <ListItem
+        primaryText={text}
+        onClick={() => { this.enableEdit() }}
+      />
     )
   }
 }
