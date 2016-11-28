cbefda419eed3718a344da6582b2a4491b4b783f
andy <andyalam@hotmail.com>
2016-11-28 00:45:16 -0800

    section 4  - completed books_list redux app

---
+++ books_list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 654 },
+    { title: 'Broken Wings', pages: 200 },
+    { title: 'Eloquent Ruby (wat)', pages: 300 }
+  ];
+}
