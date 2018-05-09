e14dd0e1c74d9aa1b1df4a93f710754a46956a26
andyalam <andyalam@hotmail.com>
2018-05-08 19:46:13 -0700

    radium and base example

--- 2018/react-complete/intro/src/App.js
+++ 2018/react-complete/intro/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -49,7 +50,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -68,6 +73,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
     const classes = [];
@@ -93,4 +102,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
