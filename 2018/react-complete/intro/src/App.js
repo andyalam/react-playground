81cbd2335f4027917a642ad11ddf20a7588c6eab
andyalam <andyalam@hotmail.com>
2018-05-08 19:53:27 -0700

    remove radium 4eva

--- 2018/react-complete/intro/src/App.js
+++ 2018/react-complete/intro/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium, { StyleRoot } from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -50,11 +49,7 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
+      cursor: 'pointer'
     };
 
     let persons = null;
@@ -73,10 +68,6 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
     }
 
     const classes = [];
@@ -88,20 +79,18 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi I'm a react app</h1>
-          <p className={classes.join(' ')}>This is working.</p>
-          <button
-            style={style}
-            onClick={this.togglePersonsHandler}>
-            Toggle Persons
-          </button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi I'm a react app</h1>
+        <p className={classes.join(' ')}>This is working.</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
