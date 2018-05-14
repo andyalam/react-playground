e39dd68f27fbf6acb454d683d8800ffa1c5125c4
andyalam <andyalam@hotmail.com>
2018-05-13 17:41:57 -0700

    fix missing keys

--- 2018/react-complete/intro/src/components/Persons/Persons.js
+++ 2018/react-complete/intro/src/components/Persons/Persons.js
@@ -5,6 +5,7 @@ import Person from './Person/Person';
 const persons = (props) => props.persons
     .map((person, index) => {
         return <Person
+            key={index}
             click={() => props.clicked(index)}
             name={person.name}
             age={person.age}
