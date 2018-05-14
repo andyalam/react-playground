76be7df79d825a3de7b37d13198c83b1493b8e2a
andyalam <andyalam@hotmail.com>
2018-05-13 17:41:25 -0700

    props ex

--- 2018/react-complete/intro/src/components/Cockpit/Cockpit.js
+++ 2018/react-complete/intro/src/components/Cockpit/Cockpit.js
@@ -15,7 +15,7 @@ const cockpit = (props) => {
 
     return (
         <div className={classes.Cockpit}>
-            <h1>Hi I'm a react app</h1>
+            <h1>{props.appTitle}</h1>
             <p className={assignedClasses.join(' ')}>This is working.</p>
             <button
                 className={btnClass}
