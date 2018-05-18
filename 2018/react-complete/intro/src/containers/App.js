93b5b3933fce21069204f4ee02832bf269c7d659
andyalam <andyalam@hotmail.com>
2018-05-17 21:57:55 -0700

    more lifecycle

--- 2018/react-complete/intro/src/containers/App.js
+++ 2018/react-complete/intro/src/containers/App.js
@@ -19,6 +19,19 @@ class App extends Component {
     console.log('[App.js Inside componentDidMount');    
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] inside shouldComponentUpdate', nextProps, nextState);
+    return true;
+}
+
+componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] inside componentWillUpdate', nextProps, nextState);
+}
+
+componentDidUpdate() {
+    console.log('[UPDATE App.js] inside componentDidUpdate');
+}
+
   state = {
     persons: [
       { id: 1, name: 'Andy', age: 22 },
