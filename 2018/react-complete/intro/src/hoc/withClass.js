010ab4ed2310467730e6e89de405ff513e41ced6
andyalam <andyalam@hotmail.com>
2018-05-23 09:23:25 -0700

    stateless/stateful hoc examples

--- 2018/react-complete/intro/src/hoc/withClass.js
+++ 2018/react-complete/intro/src/hoc/withClass.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+// stateless
+// const withClass = (WrappedComponent, className) => {
+//     return (props) => (
+//         <div className={className}>
+//             <WrappedComponent {...props} />
+//         </div>
+//     );
+// };
+
+// stateful
 const withClass = (WrappedComponent, className) => {
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent {...props} />
-        </div>
-    );
+    return class extends Component {
+        render() {
+            return (
+                <div className={className}>
+                    <WrappedComponent {...this.props} />
+                </div>
+            )
+        }
+    }
 };
 
 export default withClass;
