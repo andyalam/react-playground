f4cf6768f5b450a1602d57f3ff28a1af57ccd7d1
andyalam <andyalam@hotmail.com>
2018-06-21 11:20:53 -0700

    removing ingredients and checking for disabled state

--- 2018/react-complete/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 2018/react-complete/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -41,15 +41,42 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredientHandler = (type) => {
-        
+        // count
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        // price
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        // state
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice
+        });
     }
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
         return (
             <Fragment>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
-                    ingredientAdded={this.addIngredientHandler} />
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo} />
             </Fragment>
         );
     }
