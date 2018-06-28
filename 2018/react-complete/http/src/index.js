fad5cbc78a2b42525fa973fa6f0c07ea95967de0
andyalam <andyalam@hotmail.com>
2018-06-28 12:42:31 -0700

    axios interceptors

--- 2018/react-complete/http/src/index.js
+++ 2018/react-complete/http/src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+axios.interceptors.request.use(
+    request => {
+        console.log(request);
+        // Edit request config
+        return request;
+    },
+    error => {
+        // if the request settings fail, NOT when the request fails
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.response.use(
+    request => {
+        console.log(request);
+        // Edit request config
+        return request;
+    },
+    error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
