fd6ac940c7382da7021c54c81ef7586d06780356
andyalam <andyalam@hotmail.com>
2018-07-08 16:59:01 -0700

    guard, 404

--- 2018/http/src/containers/Blog/Blog.js
+++ 2018/http/src/containers/Blog/Blog.js
@@ -6,6 +6,9 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost'
 
 class Blog extends Component {
+    state = {
+        auth: true
+    };
 
     render () {
         return (
@@ -31,9 +34,10 @@ class Blog extends Component {
                 </header>
                 {/* loads the first matched route */}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    { this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts"  />
+                    <Route render={() => <h1>Not Found</h1>} />
+                    {/* <Redirect from="/" to="/posts"  /> */}
                 </Switch>
             </div>
         );
