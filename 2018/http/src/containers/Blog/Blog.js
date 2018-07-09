8c3755f59c17fda1443010483758531f8e4752cb
andyalam <andyalam@hotmail.com>
2018-07-09 15:53:20 -0700

    lazy loading components with hoc

--- 2018/http/src/containers/Blog/Blog.js
+++ 2018/http/src/containers/Blog/Blog.js
@@ -3,7 +3,10 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost'
+import asyncComponent from '../../hoc/asyncComponent';
+
+// import NewPost from './NewPost/NewPost'
+const AsyncNewPost = asyncComponent(() => import('./NewPost/NewPost'));
 
 class Blog extends Component {
     state = {
@@ -34,7 +37,7 @@ class Blog extends Component {
                 </header>
                 {/* loads the first matched route */}
                 <Switch>
-                    { this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
+                    { this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not Found</h1>} />
                     {/* <Redirect from="/" to="/posts"  /> */}
