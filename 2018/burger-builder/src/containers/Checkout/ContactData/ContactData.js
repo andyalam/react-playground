f9b146e2aa80433982ef01ba5be83ddc3da5ecdb
andyalam <andyalam@hotmail.com>
2018-07-17 15:49:01 -0700

    data being submitted

--- 2018/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ 2018/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -68,9 +68,16 @@ class ContactData extends Component {
     this.setState({
       loading: true
     });
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] =
+        this.state.orderForm[formElementIdentifier].value;
+    }
+
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
+      orderData: formData
     };
     
     axios
@@ -82,7 +89,19 @@ class ContactData extends Component {
       .catch(error => {
         this.setState({ loading: false });
       });
-    console.log(this.props.ingredients);
+  }
+
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+    this.setState({ orderForm: updatedOrderForm });
   }
 
   render() {
@@ -99,13 +118,14 @@ class ContactData extends Component {
         key={formElement.id}
         elementType={formElement.config.elementType}
         elementConfig={formElement.config.elementConfig}
-        value={formElement.config.value} />
+        value={formElement.config.value}
+        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
     ));
 
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {formElements}
-        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+        <Button btnType="Success">ORDER</Button>
       </form>
     );
     if (this.state.loading) {
