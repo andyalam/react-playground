bb88504743a3ec7b4db88a2e0b8f92c25f867851
Andy <andyalam9@gmail.com>
2018-10-14 10:03:58 -0700

    local state vs redux state with adding custom persons

--- 2018/exercises/redux--assignment-2/src/components/AddPerson/AddPerson.js
+++ 2018/exercises/redux--assignment-2/src/components/AddPerson/AddPerson.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './AddPerson.css';
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
-);
+class AddPerson extends Component {
+    state = {
+        name: '',
+        age: ''
+    }
 
-export default addPerson;
+    nameChangeHandler = (event) => {
+        this.setState({ name: event.target.value });
+    }
+
+    ageChangeHandler = (event) => {
+        this.setState({ age: event.target.value });
+    }
+
+    render() {
+        const { age, name } = this.state;
+        return (
+            <div className="AddPerson">
+                <input
+                    type="text"
+                    placeholder="Name"
+                    onChange={this.nameChangeHandler}
+                    value={name} />
+                <input
+                    type="number"
+                    placeholder="Age"
+                    onChange={this.ageChangeHandler}
+                    value={age} />
+                <button
+                    onClick={() => this.props.personAdded(name, age)}>
+                    Add Person
+                </button>
+            </div>
+        );
+    }
+}
+
+export default AddPerson;
--- 2018/exercises/redux--assignment-2/src/store/actions.js
+++ 2018/exercises/redux--assignment-2/src/store/actions.js
@@ -1,11 +1,11 @@
 export const ADD_PERSON = 'ADD_PERSON';
 export const DELETE_PERSON = 'DELETE_PERSON';
 
-export function onAddPerson() {
+export function onAddPerson(name, age) {
     const person = {
         id: Math.random(), // not really unique but good enough here!
-        name: 'Max',
-        age: Math.floor( Math.random() * 40 )
+        name,
+        age
     };
     return { type: ADD_PERSON, payload: { person } };
 }
