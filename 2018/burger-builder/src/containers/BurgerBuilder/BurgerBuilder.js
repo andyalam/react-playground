c7385f406f4c0f7c4ac2c35be1c5e05f1db13499
Andy <andyalam9@gmail.com>
2018-10-16 09:44:52 -0700

    redux all the things

--- 2018/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 2018/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -45,20 +45,7 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        const queryParams = [];
-        for (let i in this.props.ingredients) {
-            const qs = encodeURIComponent(i)
-                     + '='
-                     + encodeURIComponent(this.props.ingredients[i])
-            queryParams.push(qs);
-        }
-        queryParams.push('price=' + this.props.totalPrice);
-        const queryString = queryParams.join('&');
-
-        this.props.history.push({
-            pathname: '/checkout',
-            search: `?${queryString}`
-        });
+        this.props.history.push('/checkout');
     }
 
     render() {
