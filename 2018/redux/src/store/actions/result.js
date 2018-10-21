7c61ef121c2250accf94d3d930b903d00d80c098
Andy <andyalam9@gmail.com>
2018-10-21 13:50:36 -0700

    getstate as a fallback if needed, example

--- 2018/redux/src/store/actions/result.js
+++ 2018/redux/src/store/actions/result.js
@@ -1,11 +1,19 @@
 import * as actionTypes from './actionTypes';
 
 const saveResult = (count) => {
-    return { type: actionTypes.STORE_RESULT, payload: { count } };
-}
+    return {
+        type: actionTypes.STORE_RESULT,
+        payload: { count }
+    };
+};
+
 export function storeResult(count) {
-    return dispatch => {
+    return (dispatch, getState) => {
         setTimeout(() => {
+            // You can do this, but why?
+            // Just do this in the reducer. or pass in a param.
+            // const oldCounter = getState().counter.counter;
+            // console.log(oldCounter);
             dispatch(saveResult(count));
         }, 2000);
     };
