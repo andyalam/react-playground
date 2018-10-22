417fe44c1cd8ddf4b07ade9d421deebf186d5f65
Andy <andyalam9@gmail.com>
2018-10-22 14:08:37 -0700

    example of how to make leaner reducers, overkill here but good to use on larger reducers

--- 2018/redux/src/store/reducers/result.js
+++ 2018/redux/src/store/reducers/result.js
@@ -8,20 +8,27 @@ const initialState = {
     results: []
 };
 
+const storeResult = (state, action) => {
+    return updateObject(state, {
+        results: state.results.concat({
+            id: new Date(),
+            value: action.payload.count
+        })
+    })
+};
+
+const deleteResult = (state, action) => {
+    const updatedArray = state.results
+        .filter(result => result.id !== action.payload.id);
+    return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case STORE_RESULT:
-            return updateObject(state, {
-                results: state.results.concat({
-                    id: new Date(),
-                    value: action.payload.count
-                })
-            });
+            return storeResult(state, action);
         case DELETE_RESULT:
-            return updateObject(state, {
-                results: state.results
-                    .filter(result => result.id !== action.payload.id)
-            });
+            return deleteResult(state, action);
         default:
             return state;
     }
