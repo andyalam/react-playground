ef34cf613e4a4580c677491f310857b187553949
Andy <andyalam9@gmail.com>
2019-12-26 17:18:53 -0800

    routing init

---
+++ 2020/modern-react-with-redux/streams/client/src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo">PageTwo</Link>
+    </div>
+  );
+};
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo <Link to="/">PageOne</Link>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
