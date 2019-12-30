d8acf55aa3bd639aaf1ad21ffc1cf58617e97ca8
Andy <andyalam9@gmail.com>
2019-12-30 15:28:46 -0800

    context example

---
+++ 2020/modern-react-with-redux/translate/src/components/Button.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import LanguageContext from '../contexts/LanguageContext';
+
+class Button extends React.Component {
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
+  render() {
+    return (
+      <button className="ui primary button">
+        <LanguageContext.Consumer>
+          {value => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+}
+
+export default Button;
