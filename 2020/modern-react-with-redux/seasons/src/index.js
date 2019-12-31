f56974ab49905060a1f41878154e0899a3834de7
Andy <andyalam9@gmail.com>
2019-12-30 18:46:30 -0800

    refactored seasons app to use hooks

--- 2020/modern-react-with-redux/seasons/src/index.js
+++ 2020/modern-react-with-redux/seasons/src/index.js
@@ -3,51 +3,21 @@ import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import useLocation from './useLocation';
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' };
+const App = () => {
+  const [lat, errorMessage] = useLocation();
 
-  // Called reight after content visible on screen (one time, after first render)
-  componentDidMount() {
-    console.log('Component was rendered');
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request" />;
   }
 
-  // Sits and waits for updates
-  componentDidUpdate() {
-    console.log('Component was just updated - it rerendered');
-  }
-
-  // Sits and waits until this component is no longer shown
-  componentWillUnmount() {
-    console.log('My component was just updated - it rerendered');
-  }
-
-  /**
-   * Infrequently used lifecycle:
-   *
-   * shouldComponentUpdate
-   * getDerivedStateFromProps
-   * getSnapshotBeforeUpdate
-   */
-
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-
-    return <Spinner message="Please accept location request" />;
-  }
-
-  render() {
-    return <div className="border red">{this.renderContent()}</div>;
-  }
-}
+  return <div className="border red">{content}</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
